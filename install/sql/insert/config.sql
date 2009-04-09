2a7cdaf9b4bd986413929e126c56004ee05a2dab
mrh <mrh@993693f5-bd21-2247-966d-1f709dc436f8>
2009-04-09 15:08:15 +0000

    [ADD] Aktualisieren des Installationsscripts
    git-svn-id: https://localhost:8443/svn/HpClass@65 993693f5-bd21-2247-966d-1f709dc436f8

--- install/sql/insert/config.sql
+++ install/sql/insert/config.sql
@@ -1,7 +1,8 @@
 INSERT INTO `#!-PRÄFIX-!#config` (`ID`, `name`, `ok`, `description`, `typ`) VALUES
-(1, 'checkversion', 'true', 'Überprüft ob die aktuelle Version die aktuellste ist.', 'bool'),
+(1, 'checkversion', 'false', 'Überprüft ob die aktuelle Version die aktuellste ist.', 'bool'),
 (2, 'design', 'default', 'Aktuelles Design (Leer lassen für Standard)', 'string'),
 (3, 'titel', 'HPClass Demo', 'Der Webseiten Titel  (Leer lassen für Standard)', 'string'),
 (4, 'redirectlock', 'admin, config, rights', 'Seiten die von der Weiterleitung (Modul) ausgenommen sind (Mit , trennen)', 'string'),
 (5, 'superadmin', '', 'Definiert Superadmins, neben admin (Mit , trennen)', 'string'),
-(6, 'standardsite', 'news', 'Definiert die Standard Seite (Wenn Leer dann news)', 'string');
+(6, 'standardsite', 'news', 'Definiert die Standard Seite (Wenn Leer dann news)', 'string'),
+(7, 'mainheadline', '&lt;font size=''2'' face=''Comic Sans MS''>Beispieltext&lt;/font>', 'HTML - Hauptüberschrifft (nicht in allen Designs verwendet)', 'string');
