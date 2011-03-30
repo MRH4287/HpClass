284ddfe33a91fdfbf9bf2b24d7e59d5015aab914
unknown <MRH@.(none)>
2011-03-30 10:10:41 +0200

    RNew Subpage-Designb n - Register

--- sites/register.php
+++ sites/register.php
@@ -15,7 +15,7 @@ $info = $hp->getinfo();
 
 if (($_SERVER['HTTP_HOST'] == "localhost") or ($_SERVER['HTTP_HOST'] == "127.0.0.1"))
 {
-$local = true;
+  $local = true;
 }
 
 
@@ -31,144 +31,61 @@ $mail['mailfooter']="\n \r Vielen dank für ihr interesse!";
 
 
 //require_once( './include/captcha.class.php' );
-if (!isset($post['register'])) {
-?>
-<form method="POST" action="index.php?site=register">
-  <table border="0" width="522">
-    <tr>
-      <td colspan="2" width="512">
-        <p align="center"><font size="5">Anmeldung:</font></td>
-    </tr>
-    <tr>
-      <td width="150"></td>
-      <td width="365"></td>
-    </tr>
-    <tr>
-      <td width="150">Username: *+#</td>
-      <td width="365"><input type="text" name="username" size="20" maxlength="20"></td>
-    </tr>
-    <tr>
-      <td width="150">Passwort: *+#</td>
-      <td width="365"><input type="password" name="passwort12" size="20" maxlength="20"></td>
-    </tr>
-    <tr>
-      <td width="150">Passwort wiederholen:</td>
-      <td width="365"><input type="password" name="passwort212" size="20" maxlength="20"></td>
-    </tr>
-    <tr>
-      <td width="150">Vorname: *+</td>
-      <td width="365"><input type="text" name="name" size="20" maxlength="20"></td>
-    </tr>
-    <tr>
-      <td width="150">Nachname: *+</td>
-      <td width="365"><input type="text" name="nachname" size="20" maxlength="20"></td>
-    </tr>
-    <tr>
-      <td width="150">E-Mail: *</td>
-      <td width="365"><input type="text" name="email" size="20"></td>
-    </tr>
-    
-    <?php  // [ADD] 21.04.08 ändern der Registrieren page!  ?>
-      <tr>
-      <td width="150">Wohnort: *+</td>
-      <td width="365"><input type="text" name="wohnort" size="20"  maxlength="20"></td>
-    </tr>
-       <tr>
-      <td width="150">Tel. oder Handy: +</td>
-      <td width="365"><input type="text" name="tel" size="20"  maxlength="20"></td>
-    </tr>  
-      <tr>
-      <td width="150">Geschlecht: *</td>
-      <td width="365"><input type="radio" value="M" checked name="geschlecht">Mänlich / <input type="radio" name="geschlecht" value="W"> Weiblich</td>
-    </tr> 
-    
-        <?php /* ?>
-    <tr>
-      <td width="150">Bewerbungstext: *</td>
-      <td width="365" rowspan="2"><textarea rows="2" name="btext" cols="44"></textarea></td>
-    </tr>
-    <? */ ?>
-    <tr>
-    </tr>
-    <?php
-
-    if (isset($captcha)) {  ?>
-    <tr>
-    <td width="150"></td>
-    <td width="365" rowspan="2"><?php
-
-$c	= new Captcha();
-echo '<img src="./include/captcha.php?id=' . $c->getId() . '" alt="captcha" />';
-echo '<input type="hidden" name="id" value="' . $c->getId() . '" />';
-
-?>
-
-
-<input type="text" name="captcha" /><br /></td>
-
-    </tr>
-    <?php } ?>
-    <tr>
-      <td width="136"></td>
-    </tr>
-    <tr>
-      <td width="506" colspan="2"></td>
-    </tr>
-    <tr>
-      <td width="506" colspan="2">
-        <p align="center"><input type="submit" value="Jetzt registrieren!" name="register"><br><br>
-        <font size="2">* Pflichtfeld<br>+  Maximal 20 Zeichen<br>#  Mindestens 4 Zeichen</font></td>
-    </tr>
-  </table>
-</form>
-<?php } else
+if (!isset($post['register'])) 
 {
-if ($post['passwort12'] == $post['passwort212'])
+  $site = new siteTemplate($hp);
+  $site->load("register");
+  $site->display();
+
+
+} else
 {
+  if ($post['passwort12'] == $post['passwort212'])
+  {
+
+    $ok=true;
+    if (!isset($post['passwort12']) or !isset($post['username']) or !isset($post['name']) or (strlen($post['name']) < 1) or
+      !isset($post['nachname']) or (strlen($post['nachname']) < 1) or !isset($post['wohnort'])or (strlen($post['wohnort']) < 1)
+       or !isset($post['geschlecht']) or !isset($post['tel'])) 
+       { 
+          $error->error("Geben die alle Werte an!<br><a href=index.php?site=register>zurück</a><br>", "1");
+          $ok=false;
+       } 
+
+    $passwort12=$post['passwort12'];
+    $user=$post['username'];
+    $name=$post['name'];
+    $nachname=$post['nachname'];
+    $email = $post['email'];
 
 
-$ok=true;
-if (!isset($post['passwort12']) or !isset($post['username']) or !isset($post['name']) or (strlen($post['name']) < 1) or
- !isset($post['nachname']) or (strlen($post['nachname']) < 1) or !isset($post['wohnort'])or (strlen($post['wohnort']) < 1)
-   or !isset($post['geschlecht']) or !isset($post['tel'])) { 
-echo "Geben die alle Werte an!<br><a href=index.php?site=register>zurück</a><br>";
-$ok=false;
-} 
-
-$passwort12=$post['passwort12'];
-$user=$post['username'];
-$name=$post['name'];
-$nachname=$post['nachname'];
-$email = $post['email'];
-
-
-$passwort12 = str_replace('<',"&lt;" ,$passwort12);
-$user = str_replace('<',"&lt;" ,$user);
-$user = str_replace(' ',"_" ,$user);
-$name = str_replace('<',"&lt;" ,$name);
-$name = str_replace(' ',"_" ,$name);
-$nachname = str_replace('<',"&lt;" ,$nachname);
-$email = str_replace('<',"&lt;" ,$email);
-$datum = date('j').".".date('n').".".date('y');
-// [ADD] 21.04.08 ändern der Registrieren page! 
-$tel = $post['tel'];
-$wohnort = $post['wohnort'];
-$geschlecht = $post['geschlecht'];
-$tel = str_replace('<',"&lt;" ,$nachname);
-$wohnort = str_replace('<',"&lt;" ,$wohnort);
-$geschlecht = str_replace('<',"&lt;" ,$geschlecht);
+    $passwort12 = str_replace('<',"&lt;" ,$passwort12);
+    $user = str_replace('<',"&lt;" ,$user);
+    $user = str_replace(' ',"_" ,$user);
+    $name = str_replace('<',"&lt;" ,$name);
+    $name = str_replace(' ',"_" ,$name);
+    $nachname = str_replace('<',"&lt;" ,$nachname);
+    $email = str_replace('<',"&lt;" ,$email);
+    $datum = date('j').".".date('n').".".date('y');
+    // [ADD] 21.04.08 ändern der Registrieren page! 
+    $tel = $post['tel'];
+    $wohnort = $post['wohnort'];
+    $geschlecht = $post['geschlecht'];
+    $tel = str_replace('<',"&lt;" ,$nachname);
+    $wohnort = str_replace('<',"&lt;" ,$wohnort);
+    $geschlecht = str_replace('<',"&lt;" ,$geschlecht);
 
 
 
-if (strlen($user) <= 3)
-{
-echo "Der Benutzername muss mindestens 4 Zeichen haben!<br>";
-$ok = false;
-} elseif (strlen($passwort12) <= 3)
-{
-echo "Das Passwort muss mindestens 4 Zeichen haben!<br>";
-$ok = false;
-}
+    if (strlen($user) <= 3)
+    {
+      $error->error("Der Benutzername muss mindestens 4 Zeichen haben!<br>", "1");
+      $ok = false;
+    } elseif (strlen($passwort12) <= 3)
+    {
+      $error->error("Das Passwort muss mindestens 4 Zeichen haben!<br>", "1");
+      $ok = false;
+    }
 
 
 
@@ -185,94 +102,89 @@ $ok = false;
 	} 
 } */
 
-$abfrage = "SELECT * FROM ".$dbpräfix."user";
-$ergebnis = $hp->mysqlquery($abfrage);
+    $abfrage = "SELECT * FROM ".$dbpräfix."user";
+    $ergebnis = $hp->mysqlquery($abfrage);
+        
+    while($row = mysql_fetch_object($ergebnis))
+       {
+        if ((strtolower($user) == strtolower("$row->user")) or (strtolower($email) == strtolower("$row->email")))
+        {  
+          $error->error("Benutzername bereits vorhanden!<br>","1");
+          $ok=false;
+        }
+       }
     
-while($row = mysql_fetch_object($ergebnis))
-   {
-   if ((strtolower($user) == strtolower("$row->user")) or (strtolower($email) == strtolower("$row->email")))
-   {
-   echo "Benutzername bereits vorhanden!<br>";
-   $ok=false;
-   }
-   }
+    $abfrage = "SELECT * FROM ".$dbpräfix."anwaerter";
+    $ergebnis = $hp->mysqlquery($abfrage);
+        
+    while($row = mysql_fetch_object($ergebnis))
+       {
+        if (strtolower($user) == strtolower("$row->user")  or (strtolower($email) == strtolower("$row->email")))
+        {
+          $error->error("Benutzername bereits vorhanden!<br>","1");
+          $ok=false;
+        }
+       }
+  
+    if ($ok == true) 
+    {  
+      $text = "Feature Disabled";
+  
+      $passwort12 = md5($passwort12);
+      
+      $password_length = 5;
+      $generated_password = "";
+      $valid_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      $i = 0;
+  
+      for ($a = 0; $a < 5; $a++) 
+      {
+       $chars_length = strlen($valid_characters) - 1;
+       for($i = $password_length; $i--; ) 
+       {
+        $generated_password .= $valid_characters[mt_rand(0, $chars_length)];
+       }
+      	if ($a != 4)
+      	{
+          $generated_password .= "-";
+        }
+      	
+      }
+      $code = $generated_password;
+  
+  
+  
+      $eintrag = "INSERT INTO `".$dbpräfix."anwaerter`
+      (user, pass, name, nachname, datum, text, email, geschlecht, wohnort, tel, code)
+      VALUES
+      ('$user', '$passwort12', '$name', '$nachname', '$datum', '$text', '$email', '$geschlecht', '$wohnort', '$tel', '$code')";
+      $eintragen = $hp->mysqlquery($eintrag);
 
-$abfrage = "SELECT * FROM ".$dbpräfix."anwaerter";
-$ergebnis = $hp->mysqlquery($abfrage);
-    
-while($row = mysql_fetch_object($ergebnis))
-   {
-   if (strtolower($user) == strtolower("$row->user")  or (strtolower($email) == strtolower("$row->email")))
-   {
-   echo "Benutzername bereits vorhanden!<br>";
-   $ok=false;
-   }
-   }
+      $site = new siteTemplate($hp);
+      $site->load("info");
+      
+      $msg = "Antrag erfolgreich gestellt!<br>Eine Bestätigungs E-Mail ist zu ihnen Unterwegs!<br><a href=index.php>zurück</a>";
+      
+  
+  
+      if (!$local)
+      {
+       mail($email, $mail['mailbetreff'], $mail['mailtext'].$mail['mailtext'].$mail['pageadress']."?site=mailagree&user=$user&code=$code ".$mail['mailfooter'] ,"from:".$mail['mailcomefrom']);
+      } else
+      {
+        $mdg .= "<br>Diese Funktion ist auf Localhost deaktiviert!<br>gehen Sie bitte auf folgende Seite:<br>";
+        $msg .= '<a href='.$mail['pageadress']."?site=mailagree&user=$user&code=$code>".$mail['pageadress']."?site=mailagree&user=$user&code=$code</a>";
+       
+      }
+  
+      $site->set("info",$msg);
+      $site->display();
 
-if ($ok == true) {  
-$text = "Feature Disabled";
-
-$passwort12 = md5($passwort12);
-
-$password_length = 5;
-$generated_password = "";
-$valid_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-$i = 0;
-
-for ($a = 0; $a < 5; $a++) {
- $chars_length = strlen($valid_characters) - 1;
- for($i = $password_length; $i--; ) {
-  $generated_password .= $valid_characters[mt_rand(0, $chars_length)];
- }
-	if ($a != 4)
-	{
-  $generated_password .= "-";
+    }
+  } else 
+  { 
+    $error->error("Die Passwörter sind nicht identisch!<br><a href=index.php?site=register>zurück</a>","1"); 
   }
-	
-}
- $code = $generated_password;
-
-
-
-$eintrag = "INSERT INTO `".$dbpräfix."anwaerter`
-(user, pass, name, nachname, datum, text, email, geschlecht, wohnort, tel, code)
-VALUES
-('$user', '$passwort12', '$name', '$nachname', '$datum', '$text', '$email', '$geschlecht', '$wohnort', '$tel', '$code')";
-$eintragen = $hp->mysqlquery($eintrag);
-if ($eintragen== true) { 
-echo "Antrag erfolgreich gestellt!<br>Eine Bestätigungs E-Mail ist zu ihnen Unterwegs!<br><a href=index.php>zurück</a>";
-
-
-//mail("$email", "Bewerbung zur ...", "Herzich willkommen bei uns! \nDrücken sie unten stehenden Link,".
-//" um die Registrierung abzuschließen: \n<a href=\"".$pageadress."index.php?site=mailagree&user=$user>".
-//"Aktivieren</a>");
-
-//Registrierung:
-
-if (!$local)
-{
-
-
-
-
-
- mail($email, $mail['mailbetreff'], $mail['mailtext'].$mail['mailtext'].$mail['pageadress']."?site=mailagree&user=$user&code=$code ".$mail['mailfooter'] ,"from:".$mail['mailcomefrom']);
-} else
-{
-echo "<br>Diese Funktion ist auf Localhost deaktiviert!<br>gehen Sie bitte auf folgende Seite:<br>";
-echo '<a href='.$mail['pageadress']."?site=mailagree&user=$user&code=$code>".$mail['pageadress']."?site=mailagree&user=$user&code=$code</a>";
-
-}
-
-?>
-
-
-
-<?php
-}
-else{echo "Fehler: ".mysql_error(); }
-}
-} else { echo "Die Passwörter sind nicht identisch!<br><a href=index.php?site=register>zurück</a>"; }
 }
 
 ?>
