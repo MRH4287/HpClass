7c8e16971e9733ab4a21f1f091bc45378b25e0ce
Manuel Holzhauser <mrh_12@web.de>
2011-05-01 20:00:37 +0200

    ADD BeispielDatei für imageData

---
+++ template/default/imageData.php
@@ -0,0 +1,19 @@
+<?php
+
+  $sourceList = array(
+    
+    /*
+      "usr" => array(
+        
+        "table" => "user",
+        "index" => "id",
+        "dataset" => "bild",
+        "widthSet" => "width",
+        "heightSet" => "height",
+        "wantedWidth" => 150,
+        "wantedHeight" => null  
+      )
+      */
+    );
+
+?>
