7088d429a6900a466dfa9e8564ec7e142429c68c
Manuel Holzhauser <mrh_12@web.de>
2011-06-12 18:55:04 +0200

    Code Cleanup
    Signed-off-by: Manuel Holzhauser <mrh_12@web.de>

--- include/class_error.php
+++ include/class_error.php
@@ -1,105 +1,108 @@
 <?php
 class errorclass
 {
-var $errorm;
-var $errorarray = array();
-var $hp;
-var $firephp;
+  var $errorm;
+  var $errorarray = array();
+  var $hp;
+  var $firephp;
+  
+  function error($string, $l = "2", $function = "")
+  {
+  
+    if (!is_object($this->firephp))
+    {
+      $this->firephp = $this->hp->getfirephp();
+    }
+    
+    $l = (string) $l;
+    if ($l == "1")
+    {
+      $this->firephp->warn($string, $fu);
+      echo $string;
+      
+    } elseif ($l == "2")
+    {
+    
+      $this->errorm = $string;
+      
+      if (!in_array($string, $this->errorarray))
+      {
+      
+        $this->firephp->warn($string, $function);
+        array_push($this->errorarray, $string);
+        
+      }
+      
+    
+    
+    } elseif ($l == "3")
+    {
+    
+      $datum = date('j').".".date('n').".".date('y');
+      $this->firephp->error($string, $fu);
+      
+      echo "<font color=red>$string</font>";
+      
+      $this->hp->PM('mrh', 'System', 'ERROR der Stufe 3!', $string, $datum);
 
-function error($string, $l = "2", $fu = "")
-{
-
-if (!is_object($this->firephp))
-{
-$this->firephp = $this->hp->getfirephp();
+      exit;
+      
+    }
+  }
+  
+  function geterror()
+  {
+    return $this->errorm;
+  }
+  
+  function outputdiv()
+  {
+  
+    echo '<div id="errordiv" style="color:white; background-color:red; display:none;"><p align="center"></p></div>'."\n";
+  
+  }
+  
+  function showerrors()
+  {
+  
+    $string="";
+    $errorarray = $this->errorarray;
+    
+    
+    
+    foreach ($errorarray as $key=>$value) 
+    {
+      if ($string <> "")
+      {
+        $string = $string . ", ". $value;	
+      } else
+      {
+        $string = $value;
+      }
+    }
+    if ($string <> "")
+    {
+      $string = str_replace("'", '"', $string);
+      ?>
+      
+      <script type="text/javascript">
+      
+      var errordiv = document.getElementById('errordiv');
+      errordiv.innerHTML = '<p align="center">ERROR: <?php echo $string?></p>';
+      errordiv.style.display = '';
+      
+      </script>
+      
+      <?php
+    
+    }
+  
+  }
+  
+  function sethp($hp)
+  {
+    $this->hp=$hp;
+  }
+  
 }
-
-$l = (string) $l;
-if ($l == "1")
-{
-$this->firephp->warn($string, $fu);
-
-echo $string;
-} elseif ($l == "2")
-{
-
-$this->errorm = $string;
-
-if (!in_array($string, $this->errorarray))
-{
-$this->firephp->warn($string, $fu);
-
-array_push($this->errorarray, $string);
-}
-
-
-
-} elseif ($l == "3")
-{
-$datum = date('j').".".date('n').".".date('y');
-$this->firephp->error($string, $fu);
-
-echo "<font color=red>$string</font>";
-
-$this->hp->PM('mrh', 'System', 'ERROR der Stufe 3!', $string, $datum);
-
-
-exit;
-}
-}
-
-function geterror()
-{
-return $this->errorm;
-}
-
-function outputdiv()
-{
-
-echo '<div id="errordiv" style="color:white; background-color:red; display:none;"><p align="center"></p></div>'."\n";
-
-}
-
-function showerrors()
-{
-$string="";
-$errorarray = $this->errorarray;
-
-
-
-foreach ($errorarray as $key=>$value) {
-if ($string <> "")
-{
-$string = $string . ", ". $value;	
-} else
-{
-$string = $value;
-}
-}
-if ($string <> "")
-{
-$string = str_replace("'", '"', $string);
-?>
-
-<script type="text/javascript">
-
-var errordiv = document.getElementById('errordiv');
-errordiv.innerHTML = '<p align="center">ERROR: <?php echo $string?></p>';
-errordiv.style.display = '';
-
-</script>
-
-<?php
-
-}
-
-}
-
-function sethp($hp)
-{
-$this->hp=$hp;
-}
-
-}
-
 ?>
--- include/class_info.php
+++ include/class_info.php
@@ -2,128 +2,124 @@
 
 class infoclass
 {
-var $lang;
-var $error;
-var $hp;
-var $infoarray = array();
-var $okmarray = array();
-
-var $firephp;
-
-function init($lang, $error, $hp)
-{
-$this->lang = $lang;
-$this->error = $error;
-$this->hp = $hp;
-$this->firephp = $hp->getfirephp();
-}
-
-function info($info)
-{
- if (!in_array($info, $this->infoarray))
+  var $lang;
+  var $error;
+  var $hp;
+  var $infoarray = array();
+  var $okmarray = array();
+  
+  var $firephp;
+  
+  function init($lang, $error, $hp)
   {
-  array_push($this->infoarray, $info);
-  $this->firephp->info($info);
+    $this->lang = $lang;
+    $this->error = $error;
+    $this->hp = $hp;
+    $this->firephp = $hp->getfirephp();
   }
-}
-
-function okm($okm)
-{
- if (!in_array($okm, $this->okmarray))
+  
+  function info($info)
   {
-  array_push($this->okmarray, $okm);
-  $this->firephp->log($okm);
+   if (!in_array($info, $this->infoarray))
+    {
+      array_push($this->infoarray, $info);
+      $this->firephp->info($info);
+    }
   }
+  
+  function okm($okm)
+  {
+   if (!in_array($okm, $this->okmarray))
+    {
+      array_push($this->okmarray, $okm);
+      $this->firephp->log($okm);
+    }
+  }
+  
+  function okn($okm)
+  {
+    // Löst das Problem, dass ich mich beim Programmieren verschreib :-D
+    $this->okm($okm);
+  }
+  
+  
+  function outputdiv()
+  {
+  
+    echo '<div id="infodiv" style="color:black; background-color:yellow; display:none;"><p align="center"></p></div>'."\n";
+    
+    echo '<div id="okdiv" style="color:white; background-color:green; display:none;"><p align="center"></p></div>'."\n";
+    
+  }
+  
+  function getmessages()
+  {
+    $string="";
+    $infoarray = $this->infoarray;
+    
+    
+    
+    foreach ($infoarray as $key=>$value)
+    {
+      if ($string <> "")
+      {
+        $string = $string . ", ". $value;	
+      } else
+      {
+        $string = $value;
+      }
+    }
+    if ($string <> "")
+    {
+      $string = str_replace("'", '\\"', $string);
+      ?>
+      
+      <script type="text/javascript">
+      
+      var errordiv = document.getElementById('infodiv');
+      errordiv.innerHTML = '<p align="center"><?php echo $string?></p>';
+      errordiv.style.display = '';
+      
+      </script>
+      
+      <?php
+    
+    }
+    
+    
+    $string="";
+    $okmarray = $this->okmarray;
+    
+    
+    
+    foreach ($okmarray as $key=>$value) 
+    {
+      if ($string <> "")
+      {
+        $string = $string . ", ". $value;	
+      } else
+      {
+        $string = $value;
+      }
+    }
+    if ($string <> "")
+    {
+      $string = str_replace("'", '\"', $string);
+      ?>
+      
+      <script type="text/javascript">
+      
+      var errordiv = document.getElementById('okdiv');
+      errordiv.innerHTML = '<p align="center"><?php echo $string?></p>';
+      errordiv.style.display = '';
+      
+      </script>
+      
+      <?php
+    
+    }
+ 
+  }
+
 }
-
-function okn($okm)
-{
-// Löst das Problem, dass ich mich beim Programmieren verschreib :-D
-$this->okm($okm);
-}
-
-
-function outputdiv()
-{
-
-echo '<div id="infodiv" style="color:black; background-color:yellow; display:none;"><p align="center"></p></div>'."\n";
-
-echo '<div id="okdiv" style="color:white; background-color:green; display:none;"><p align="center"></p></div>'."\n";
-
-}
-
-function getmessages()
-{
-$string="";
-$infoarray = $this->infoarray;
-
-
-
-foreach ($infoarray as $key=>$value) {
-if ($string <> "")
-{
-$string = $string . ", ". $value;	
-} else
-{
-$string = $value;
-}
-}
-if ($string <> "")
-{
-$string = str_replace("'", '\\"', $string);
-?>
-
-<script type="text/javascript">
-
-var errordiv = document.getElementById('infodiv');
-errordiv.innerHTML = '<p align="center"><?php echo $string?></p>';
-errordiv.style.display = '';
-
-</script>
-
-<?php
-
-}
-
-
-$string="";
-$okmarray = $this->okmarray;
-
-
-
-foreach ($okmarray as $key=>$value) {
-if ($string <> "")
-{
-$string = $string . ", ". $value;	
-} else
-{
-$string = $value;
-}
-}
-if ($string <> "")
-{
-$string = str_replace("'", '\"', $string);
-?>
-
-<script type="text/javascript">
-
-var errordiv = document.getElementById('okdiv');
-errordiv.innerHTML = '<p align="center"><?php echo $string?></p>';
-errordiv.style.display = '';
-
-</script>
-
-<?php
-
-}
-
-
-
-
-
-}
-
-}
-
-
 ?>
