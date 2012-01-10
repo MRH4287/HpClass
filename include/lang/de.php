d21f8b80f27456df863d2cc2d3eef7ca1a4e60f7
Manuel Holzhauser <mrh_12@web.de>
2012-01-10 11:56:15 +0100

    Merge Lang-Files into one file per language

---
+++ include/lang/de.php
@@ -0,0 +1,400 @@
+<?php
+
+$lang['de'] = array(
+    // pm-de
+    // pm-eng
+    "absender" => "Absender",
+
+    // user-de
+    // user-eng
+    "alter" => "Alter",
+
+    // pm-de
+    // pm-eng
+    "antworten" => "Antworten",
+
+    // admin-de
+    // admin-eng
+    "anwerter" => "Anwerter",
+
+    // profil-de
+    // profil-eng
+    // user-de
+    // user-eng
+    "avatar" => "Avatar",
+
+    // de-eng-default
+    "back" => "Zurück",
+
+    // pm-de
+    // pm-eng
+    "betreff" => "Betreff",
+
+    // anwaerter-de
+    // anwaerter-eng
+    "bewebungstext" => "Bewebungstext",
+
+    // anwaerter-de
+    // anwaerter-eng
+    "bewerbungen" => "Bewerbungen",
+
+    // user-de
+    // user-eng
+    "birthday" => "Geburtstag",
+
+    // de-eng-default
+    "by" => "von",
+
+    // pm-de
+    // pm-eng
+    "cantdelmessage" => "Sie können diese Nachricht nicht Löschen!!",
+
+    // profil-de
+    // profil-eng
+    "changepw" => "Passwort ändern",
+
+    // profil-de
+    // profil-eng
+    "clan" => "Clan",
+
+    // profil-de
+    // profil-eng
+    "clanhist" => "Clan History",
+
+    // profil-de
+    // profil-eng
+    "clanhp" => "Clan Homepage",
+
+    // profil-de
+    // profil-eng
+    "clanname" => "Clanname",
+
+    // profil-de
+    // profil-eng
+    "clantag" => "ClanTag",
+
+    // de-eng-default
+    "comments" => "Kommentare",
+
+    // profil-de
+    // profil-eng
+    "cpu" => "CPU",
+
+    // user-de
+    // user-eng
+    "datum" => "Datum",
+
+    // de-eng-default
+    "delet" => "Löschen",
+
+    // admin-de
+    // admin-eng
+    // anwaerter-de
+    // anwaerter-eng
+    "delok" => "Erfolgreich gelöscht!",
+
+    // download-de
+    // download-eng
+    "description" => "Beschreibung",
+
+    // pm-de
+    // pm-eng
+    "doublepost" => "Doppelpost entdeckt!",
+
+    // de-eng-default
+    "edit" => "Bearbeiten",
+
+    // admin-de
+    // admin-eng
+    "editnews" => "News Bearbeiten",
+
+    // de-eng-default
+    "editprofile" => "Profil bearbeiten",
+
+    // profil-de
+    // profil-eng
+    "email" => "E-Mail Adresse",
+
+    // pm-de
+    // pm-eng
+    "empfänger" => "Empfänger",
+
+    // de-eng-default
+    "error" => "Fehler",
+
+    // admin-de
+    // admin-eng
+    "error-del" => "Fehler beim Löschen:",
+
+    // profil-de
+    // profil-eng
+    "error-login-profil" => "Sie müssen sich erst einloggen, bevor sie das Profil änder können!",
+
+    // admin-de
+    // admin-eng
+    "error-post" => "Fehler beim eintragen der News-Meldung",
+
+    // pm-de
+    // pm-eng
+    "für" => "Für",
+
+    // pm-de
+    // pm-eng
+    "gelesen" => "Gelesen",
+
+    // profil-de
+    // profil-eng
+    "graka" => "Grafik-Karte",
+
+    // profil-de
+    // profil-eng
+    "hdd" => "Festplatte(n)",
+
+    // admin-de
+    // admin-eng
+    "headline" => "Überschrift:",
+
+    // anwaerter-de
+    // anwaerter-eng
+    "kberechacanwae" => "Sie haben nicht die nötige Berechtigung, um Anwärter aufzunehmen!",
+
+    // anwaerter-de
+    // anwaerter-eng
+    "kberechdeanwae" => "Sie haben nicht die nötige Berechtigung, um Anwärter abzulehnen!",
+
+    // user-de
+    // user-eng
+    "lastlogin" => "Letzter Login",
+
+    // de-eng-default
+    "loggedas" => "Eingeloggt als:",
+
+    // de-eng-default
+    "login" => "Loggen Sie sich bitte ein!",
+
+    // de-eng-default
+    "loginbutt" => "Login",
+
+    // de-eng-default
+    "loginfail" => "Kein Login möglich. Benutzername oder Passwort sind falsch!",
+
+    // user-de
+    // user-eng
+    "mail" => "E-Mail",
+
+    // pm-de
+    // pm-eng
+    "messagenotforyou" => "Diese Nachricht ist nicht für Sie bestimmt!",
+
+    // admin-de
+    // admin-eng
+    "modulman" => "Modul Manager",
+
+    // de-eng-default
+    "morenews" => "Weitere Nachrichten",
+
+    // user-de
+    // user-eng
+    "nachname" => "Nachname",
+
+    // user-de
+    // user-eng
+    "name" => "Name",
+
+    // user-de
+    // user-eng
+    "never" => "Niemals",
+
+    // admin-de
+    // admin-eng
+    "newnews" => "Neue Nachricht:",
+
+    // pm-de
+    // pm-eng
+    "newpm" => "Neue PM verfassen",
+
+    // profil-de
+    // profil-eng
+    "newpw" => "Neues Passwort",
+
+    // user-de
+    // user-eng
+    "nick" => "Nickname",
+
+    // de-eng-default
+    "no" => "Nein",
+
+    // de-eng-default
+    "nodb" => "Die Datenbank existiert nicht.",
+
+    // admin-de
+    // admin-eng
+    "nodelnews" => "Sie dürfen keine Newsmeldungen löschen!",
+
+    // de-eng-default
+    "noright" => "Sie haben nicht das Recht das zu tun!",
+
+    // de-eng-default
+    "noright2" => "Sie haben nicht das Recht diese Seite zu betreten!",
+
+    // download-de
+    // download-eng
+    "norightlookfile" => "Sie haben nicht das nötige level diese Datei herunter zo laden!",
+
+    // profil-de
+    // profil-eng
+    "notallfields" => "Es wurden nicht alle Pflichfelder ausgefüllt",
+
+    // de-eng-default
+    "notfound" => "404 Die gewünschte Seite konnte nicht gefunden werden!",
+
+    // admin-de
+    // admin-eng
+    "nowritenews" => "Sie dürfen keine Newsmeldungen schreiben!",
+
+    // profil-de
+    // profil-eng
+    "ok" => "Bestätigen",
+
+    // profil-de
+    // profil-eng
+    "ok_profil" => "Erfolgreich Aktualisiert",
+
+    // profil-de
+    // profil-eng
+    "oldpw" => "Altes Passwort",
+
+    // user-de
+    // user-eng
+    "page" => "Seite",
+
+    // profil-de
+    // profil-eng
+    "page-intern" => "Seiten intern",
+
+    // de-eng-default
+    "password" => "Passwort",
+
+    // profil-de
+    // profil-eng
+    "persönliches" => "Persönliches",
+
+    // user-de
+    // user-eng
+    "pm" => "PM schicken",
+
+    // admin-de
+    // admin-eng
+    "post" => "Eintragen",
+
+    // pm-de
+    // pm-eng
+    "postausgang" => "Postausgang",
+
+    // pm-de
+    // pm-eng
+    "posterror" => "Fehler Melden",
+
+    // admin-de
+    // admin-eng
+    // anwaerter-de
+    // anwaerter-eng
+    "postok" => "Erfolgreich eingetragen",
+
+    // profil-de
+    // profil-eng
+    "pwwrong_profil" => "Passwort falsch eingegeben",
+
+    // admin-de
+    // admin-eng
+    "questions-webmaster" => "Sollten Sie Fragen haben, wenden die sich an den Webmaster!",
+
+    // profil-de
+    // profil-eng
+    "ram" => "RAM",
+
+    // user-de
+    // user-eng
+    "rank" => "Rang",
+
+    // de-eng-default
+    "register" => "Registrieren",
+
+    // profil-de
+    // profil-eng
+    "repnewpw" => "Passwort bestätigen",
+
+    // de-eng-default
+    "send" => "Abschicken",
+
+    // user-de
+    // user-eng
+    "since" => "Seit",
+
+    // profil-de
+    // profil-eng
+    "sonstiges" => "Sonstiges",
+
+    // profil-de
+    // profil-eng
+    "start" => "Start",
+
+    // profil-de
+    // profil-eng
+    "telephone" => "Telefon oder Handy",
+
+    // pm-de
+    // pm-eng
+    "text" => "Text",
+
+    // download-de
+    // download-eng
+    "titel" => "Titel",
+
+    // admin-de
+    // admin-eng
+    "upload" => "Dateien hochladen",
+
+    // profil-de
+    // profil-eng
+    "upload-picture" => "Bild hochladen",
+
+    // download-de
+    // download-eng
+    "uploadedam" => "Hochgeladen am",
+
+    // download-de
+    // download-eng
+    // eng
+    "uploadedby" => "Hochgeladen von",
+
+    // de-eng-default
+    "username" => "Benutzername",
+
+    // de-eng-default
+    "userorpasswordwrong" => "keine Verbindung möglich. Benutzername oder Passwort sind falsch",
+
+    // pm-de
+    // pm-eng
+    "von" => "Von",
+
+    // profil-de
+    // profil-eng
+    // user-de
+    // user-eng
+    "wohnort" => "Wohnort",
+
+    // admin-de
+    // admin-eng
+    "wrotenews" => "Sollten Sie gerade eine Newsmeldung geschrieben haben, öffnen Sie in neues Fenster und loggen Sie sich ein.",
+
+    // admin-de
+    // admin-eng
+    "wrotenews2" => "Danach gehen Sie auf diese Seite zurück und drücken Aktualisieren!",
+
+    // de-eng-default
+    "yes" => "Ja"
+
+);
+
+?>
---
+++ include/lang/eng.php
@@ -0,0 +1,402 @@
+<?php
+
+$lang['eng'] = array(
+    // pm-eng
+    // profil-de
+    "absender" => "Sender",
+
+    // user-eng
+    "alter" => "Age",
+
+    // pm-eng
+    // profil-de
+    "antworten" => "Reply",
+
+    // admin-eng
+    // anwaerter-de
+    "anwerter" => "	Awaiting Activation",
+
+    // profil-eng
+    // user-de
+    // user-eng
+    "avatar" => "Avatar",
+
+    // de-eng-default
+    // download-de
+    "back" => "back",
+
+    // pm-eng
+    // profil-de
+    "betreff" => "Subject",
+
+    // anwaerter-eng
+    "bewebungstext" => "Application",
+
+    // anwaerter-eng
+    "bewerbungen" => "Applications",
+
+    // user-eng
+    "birthday" => "Birthday",
+
+    // de-eng-default
+    // download-de
+    "by" => "by",
+
+    // pm-eng
+    // profil-de
+    "cantdelmessage" => "You can not delete this message!!",
+
+    // profil-eng
+    // user-de
+    "changepw" => "Change Password",
+
+    // profil-eng
+    // user-de
+    "clan" => "Clan",
+
+    // profil-eng
+    // user-de
+    "clanhist" => "Clan History",
+
+    // profil-eng
+    // user-de
+    "clanhp" => "Clan Homepage",
+
+    // profil-eng
+    // user-de
+    "clanname" => "Clanname",
+
+    // profil-eng
+    // user-de
+    "clantag" => "ClanTag",
+
+    // de-eng-default
+    // download-de
+    "comments" => "Comments",
+
+    // profil-eng
+    // user-de
+    "cpu" => "CPU",
+
+    // user-eng
+    "datum" => "Date",
+
+    // de-eng-default
+    // download-de
+    "delet" => "delet",
+
+    // admin-eng
+    // anwaerter-de
+    // anwaerter-eng
+    "delok" => "Successfully deleted!",
+
+    // download-eng
+    // pm-de
+    "description" => "Description",
+
+    // pm-eng
+    // profil-de
+    "doublepost" => "Double Post discovered!",
+
+    // de-eng-default
+    // download-de
+    "edit" => "edit",
+
+    // admin-eng
+    // anwaerter-de
+    "editnews" => "Edit News",
+
+    // de-eng-default
+    // download-de
+    "editprofile" => "Edit Profile",
+
+    // profil-eng
+    // user-de
+    "email" => "E-Mail Adress",
+
+    // pm-eng
+    // profil-de
+    "empfänger" => "addressee",
+
+    // de-eng-default
+    // download-de
+    "error" => "Error",
+
+    // admin-eng
+    // anwaerter-de
+    "error-del" => "Error while deleting:",
+
+    // profil-eng
+    // user-de
+    "error-login-profil" => "You must login to change your profile!",
+
+    // admin-eng
+    // anwaerter-de
+    "error-post" => "Error while posting",
+
+    // pm-eng
+    // profil-de
+    "für" => "For",
+
+    // pm-eng
+    // profil-de
+    "gelesen" => "read",
+
+    // profil-eng
+    // user-de
+    "graka" => "Graphics card",
+
+    // profil-eng
+    // user-de
+    "hdd" => "Hard Drive",
+
+    // admin-eng
+    // anwaerter-de
+    "headline" => "Headline:",
+
+    // anwaerter-eng
+    "kberechacanwae" => "You do not have the required permissions to activate members!",
+
+    // anwaerter-eng
+    "kberechdeanwae" => "You do not have the necessary authority to reject members!",
+
+    // user-eng
+    "lastlogin" => "LAst Login",
+
+    // de-eng-default
+    // download-de
+    "loggedas" => "Logged as:",
+
+    // de-eng-default
+    // download-de
+    "login" => "Please Login!",
+
+    // de-eng-default
+    // download-de
+    "loginbutt" => "login",
+
+    // de-eng-default
+    // download-de
+    "loginfail" => "Can`t Login, your Username or Password are wrong!",
+
+    // user-eng
+    "mail" => "E-Mail",
+
+    // pm-eng
+    // profil-de
+    "messagenotforyou" => "This message is not intended for you!",
+
+    // admin-eng
+    // anwaerter-de
+    "modulman" => "Modul Manager",
+
+    // de-eng-default
+    // download-de
+    "morenews" => "More Articels",
+
+    // user-eng
+    "nachname" => "family name",
+
+    // user-eng
+    "name" => "name",
+
+    // user-eng
+    "never" => "Never",
+
+    // admin-eng
+    // anwaerter-de
+    "newnews" => "Write News report:",
+
+    // pm-eng
+    // profil-de
+    "newpm" => "write new pm",
+
+    // profil-eng
+    // user-de
+    "newpw" => "New Password",
+
+    // user-eng
+    "nick" => "Nickname",
+
+    // de-eng-default
+    // download-de
+    "no" => "no",
+
+    // de-eng-default
+    // download-de
+    "nodb" => "The Database do not exsist.",
+
+    // admin-eng
+    // anwaerter-de
+    "nodelnews" => "You are not allowed to delete news!",
+
+    // de-eng-default
+    // download-de
+    "noright" => "You are not allowed to do this!",
+
+    // de-eng-default
+    // download-de
+    "noright2" => "You ae not allowed to enter this site!",
+
+    // download-eng
+    // pm-de
+    "norightlookfile" => "You do not have the necessary level this download this file!",
+
+    // profil-eng
+    // user-de
+    "notallfields" => "There were not filled in all mandatory fields",
+
+    // de-eng-default
+    // download-de
+    "notfound" => "404 The requestet page can not be found!",
+
+    // admin-eng
+    // anwaerter-de
+    "nowritenews" => "You are not allowed to write news!",
+
+    // profil-eng
+    // user-de
+    "ok" => "Confirm",
+
+    // profil-eng
+    // user-de
+    "ok_profil" => "Successfully Updated",
+
+    // profil-eng
+    // user-de
+    "oldpw" => "Old Password",
+
+    // user-eng
+    "page" => "Page",
+
+    // profil-eng
+    // user-de
+    "page-intern" => "Page Intern",
+
+    // de-eng-default
+    // download-de
+    "password" => "Password",
+
+    // profil-eng
+    // user-de
+    "persönliches" => "Personal",
+
+    // user-eng
+    "pm" => "Send PM",
+
+    // admin-eng
+    // anwaerter-de
+    "post" => "Post",
+
+    // pm-eng
+    // profil-de
+    "postausgang" => "Outbox",
+
+    // pm-eng
+    // profil-de
+    "posterror" => "Post Error Log",
+
+    // admin-eng
+    // anwaerter-de
+    // anwaerter-eng
+    "postok" => "Successfully registered",
+
+    // profil-eng
+    // user-de
+    "pwwrong_profil" => "Password is entered incorrectly",
+
+    // admin-eng
+    // anwaerter-de
+    "questions-webmaster" => "If you have any questions, contact the the webmaster!",
+
+    // profil-eng
+    // user-de
+    "ram" => "RAM",
+
+    // user-eng
+    "rank" => "Level",
+
+    // de-eng-default
+    // download-de
+    "register" => "Register",
+
+    // profil-eng
+    // user-de
+    "repnewpw" => "Confirm Password",
+
+    // de-eng-default
+    // download-de
+    "send" => "send",
+
+    // user-eng
+    "since" => "Since",
+
+    // profil-eng
+    // user-de
+    "sonstiges" => "Other",
+
+    // profil-eng
+    // user-de
+    "start" => "start",
+
+    // profil-eng
+    // user-de
+    "telephone" => "Telephone or mobile phone",
+
+    // pm-eng
+    // profil-de
+    "text" => "Text",
+
+    // download-eng
+    // pm-de
+    "titel" => "Titel",
+
+    // admin-eng
+    // anwaerter-de
+    "upload" => "Upload file",
+
+    // profil-eng
+    // user-de
+    "upload-picture" => "Upload Picture",
+
+    // download-eng
+    // pm-de
+    "uploadedam" => "Uploaded",
+
+    // download-eng
+    // pm-de
+    "uploadedby" => "Uploaded by",
+
+    // de-eng-default
+    // download-de
+    "username" => "Username",
+
+    // de-eng-default
+    // download-de
+    "userorpasswordwrong" => "no connection possible! Username or Password wrong!",
+
+    // pm-eng
+    // profil-de
+    "von" => "By",
+
+    // profil-eng
+    // user-de
+    // user-eng
+    "wohnort" => "Residence",
+
+    // admin-eng
+    // anwaerter-de
+    "wrotenews" => "If you have just written a news report, open in new window and login to your account.",
+
+    // admin-eng
+    // anwaerter-de
+    "wrotenews2" => "Then go back to this page and press Update!",
+
+    // de-eng-default
+    // download-de
+    "yes" => "yes"
+
+);
+
+
+?>
